d5f97f142fb0cd3ec6953b1d10d56115a18e89f9
admin <admin@admins-MacBook-Pro-4.local>
2020-03-16 22:11:51 +0530

    SetUp shine resume react

---
+++ desktop/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import AppRouter from './routes/index';
+
+class App extends Component {
+    render() {
+        return (
+
+            < AppRouter />
+    )
+        ;
+    }
+}
+
+export default App;
---
+++ desktop/src/components/Core/Home/home.jsx
@@ -0,0 +1,35 @@
+import React, {  useState } from 'react';
+import AddonServices from './AddonServices/addonServices.jsx'
+import Banner from  './Banner/banner.jsx'
+// import ChooseServicesSlider from './ChooseServicesSlider/chooseServicesSlider.jsx'
+// import EnquireNow from './EnquireNow/enquireNow.jsx'
+// import FAQ from './FAQ/faq.jsx'
+// import LikeCources from './LikeCourses/likeCourses.jsx'
+// import OurCustomers from './OurCustomers/ourCustomers.jsx'
+// import ProfessionalWriters from './ProfessionalWriters/professionalWriters.jsx'
+// import RelatedArticles from './RelatedArticles/relatedArticles.jsx'
+// import ResumeSlider from './ResumeSlider/resumeSlider.jsx'
+// import ServiceLog from './ServiceLog/serviceLog.jsx'
+// import SuccessStories from './SuccessStories/successStories.jsx'
+
+export default function Home() {
+    // const [buttonText, setButtonText] = useState("Click me, please");
+  
+    return (
+        <div>
+        <h2>Hello, world</h2>
+        <Banner/>
+        {/* <AddonServices/>
+        <ChooseServicesSlider/>
+        <EnquireNow/>
+        <FAQ/>
+        <LikeCources/>
+        <OurCustomers/>
+        <ProfessionalWriters/>
+        <RelatedArticles/>
+        <ResumeSlider/>
+        <ServiceLog/>
+        <SuccessStories/> */}
+        </div>
+    );
+  }
